f2681c0da0d963acd9be450e5219098bad780727
Johnny Deng <qiang1012@gmail.com>
2013-11-10 17:31:32 +0800

    appkey, appsecret

--- DianpingSDK/DPAPI.h
+++ DianpingSDK/DPAPI.h
@@ -10,8 +10,8 @@
 #import "AFHTTPSessionManager.h"
 
 #define DPAPIBaseURLString          @"http://api.dianping.com/v1/"
-#define DPAppKey                    @"45285573"
-#define DPAppSecret                 @"a405ee607aa345fca4cd8256618e9f38"
+#define DPAppKey                    @"your appKey"
+#define DPAppSecret                 @"your appSecrect"
 
 @interface DPAPI : AFHTTPSessionManager
 
